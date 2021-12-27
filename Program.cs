e4d51d86a3564cc7cc4024356174c31a319ca22d
Meeesha <81447820+D4RK21@users.noreply.github.com>
2021-12-27 16:43:26 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
