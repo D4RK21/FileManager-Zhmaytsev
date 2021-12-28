2c0a92c5517a76c9d5d4e7b8cf578786e72d5358
Meeesha <81447820+D4RK21@users.noreply.github.com>
2021-12-28 20:29:50 +0300

    Created File and Directory classes

---
+++ MyDirectory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace FileManager
+{
+    public class MyDirectory : IDisposable
+    {
+        private string _name;
+        private string _fullPath;
+        private DateTime _dateCreated;
+        private DateTime _dateLastAccess;
+        private DateTime _dateLastWriteTime;
+
+        public MyDirectory(DirectoryInfo directoryInfo)
+        {
+            _name = directoryInfo.Name;
+            _fullPath = directoryInfo.FullName;
+            _dateCreated = directoryInfo.CreationTime;
+            _dateLastAccess = directoryInfo.LastAccessTime;
+            _dateLastWriteTime = directoryInfo.LastWriteTime;
+        }
+        
+        public void Dispose()
+        {
+            Console.WriteLine("Directory object has been disposed!");    
+        }
+    }
+}
---
+++ MyFile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace FileManager
+{
+    public class MyFile : IDisposable
+    {
+        private string _name;
+        private long _size;
+        private DateTime _dateCreated;
+        private DateTime _dateUpdated;
+
+        public MyFile(FileInfo fileInfo)
+        {
+            _name = fileInfo.Name;
+            _size = fileInfo.Length;
+            _dateCreated = fileInfo.CreationTime;
+            _dateUpdated = fileInfo.LastWriteTime;
+        }
+        
+        public void Dispose()
+        {
+            Console.WriteLine("File object has been disposed!");    
+        }
+    }
+}
