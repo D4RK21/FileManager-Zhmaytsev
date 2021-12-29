906f9bf93c9752b140dae5ad83df4630fdab5567
Meeesha <81447820+D4RK21@users.noreply.github.com>
2021-12-29 17:26:18 +0300

    Add parsing input string and splitting it on command, arguments and flags

---
+++ Helper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace FileManager
+{
+    public static class Helper
+    {
+        public static void ParseInputString(string input, out string command, out string[] arguments, out string[] flags)
+        {
+            var regex = new Regex(@"("".*?""|[^ ""]+)+");
+            var matchesArray = regex.Matches(input).ToArray();
+            
+            int argumentsCounter = 0;
+            int flagsCounter = 0;
+            
+            for (int i = 1; i < matchesArray.Length; i++)
+            {
+                if (matchesArray[i].Value[0] != '-')
+                {
+                    argumentsCounter++;
+                }
+                else
+                {
+                    flagsCounter++;
+                }
+            }
+            
+            command = matchesArray[0].Value;
+            arguments = new string[argumentsCounter];
+            flags = new string[flagsCounter];
+
+            argumentsCounter = 0;
+            flagsCounter = 0;
+            
+            for (int i = 1; i < matchesArray.Length; i++)
+            {
+                if (matchesArray[i].Value[0] != '-')
+                {
+                    var argumentText = matchesArray[i].Value;
+
+                    if (argumentText[0] == '"' && argumentText[^1] == '"')
+                    {
+                        argumentText = argumentText.Substring(1, argumentText.Length - 2);
+                    }
+                    arguments[argumentsCounter] = argumentText;
+                    argumentsCounter++;
+                }
+                else
+                {
+                    flags[flagsCounter] = matchesArray[i].Value.Substring(1);
+                    flagsCounter++;
+                }
+
+            }
+        }
+        
+        public static void SortByFlag()
+        {
+            
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Drawing;
 
 namespace FileManager
 {
@@ -7,24 +8,40 @@ namespace FileManager
     {
         static void Main(string[] args)
         {
-            string path = @"../../../";
-            
-            MyDirectory directory = new MyDirectory(path);
-            Console.WriteLine(directory.GetContent());
-            
-            Console.ReadLine();
+            var currentDirectory = new MyDirectory(@"../../../");
 
-            // while (true)
-            // {
-            //     string inputText = Console.ReadLine();
-            //
-            //     switch (inputText)
-            //     {
-            //         case "cd":
-            //             
-            //             break;
-            //     }
-            // }
+            while (true)
+            {
+                var currentPath = currentDirectory.GetFullPath();
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write($"\n{currentPath.Substring(0, currentPath.Length - 1)} > ");
+                Console.ResetColor();
+
+                string inputText = Console.ReadLine()?.Trim();
+                if (string.IsNullOrEmpty(inputText)) continue;
+                string[] inputTextArr = inputText.Split(' ');
+                
+                Helper.ParseInputString(inputText, out var inputCommand,out var inputArguments, out var inputFlags);
+                
+                // if (inputText.Contains('"'))
+                // {
+                //     
+                // }
+                // else
+                // {
+                //     inputCommand = inputTextArr[0].ToLower();
+                //     inputArguments = inputTextArr[1];
+                //     inputFlags = inputTextArr[2].ToLower();
+                // }
+
+                switch (inputCommand)
+                {
+                    case "dir":
+                        Console.WriteLine(currentDirectory.GetContent(inputFlags));
+                        break;
+                }
+            }
         }
     }
 }
