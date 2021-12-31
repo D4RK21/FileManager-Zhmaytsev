660cfd27a2522465635377ffb5e81f2fbb198975
Meeesha <81447820+D4RK21@users.noreply.github.com>
2021-12-31 16:05:15 +0300

    Add unit tests

--- UnitTests/UnitTest1.cs
+++ UnitTests/UnitTest1.cs
@@ -1,12 +1,92 @@
+using FileManager;
 using Xunit;
 
-
-    public class UnitTest
+namespace UnitTest
+{
+    public class FileManagerTests
     {
         [Fact]
-        public void Test1()
+        public void GetContent_Contain_Program_cs()
         {
+            // Arrange
+            var directory = new MyDirectory(@"../../../../");
+            var flags = new string[] { };
+            var expected = "Program.cs";
             
+            // Act
+            var actual = directory.GetContent(flags);
+
+            // Assert
+            Assert.Contains(expected, actual);
+        }
+
+        [Fact]
+        public void GetContent_With_Flags()
+        {
+            // Arrange
+            var directory = new MyDirectory(@"../../../../");
+            var flags = new string[] { "t" };
+            var expected = "Creation time:";
+            
+            // Act
+            var actual = directory.GetContent(flags);
+
+            // Assert
+            Assert.Contains(expected, actual);
+        }
+        
+        [Fact]
+        public void ViewFile()
+        {
+            // Arrange
+            var file = new MyFile(@"../../../../Program.cs");
+            var expected = "using System;";
+            
+            // Act
+            var actual = file.View(200);
+
+            // Assert
+            Assert.Contains(expected, actual);
+        }
+        
+        [Fact]
+        public void Find_SubString_In_File()
+        {
+            // Arrange
+            var file = new MyFile(@"../../../../Program.cs");
+            
+            // Act
+            var result = file.IsSubstrExistInFile("using System;");
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void CreateFile()
+        {
+            // Arrange
+            var directory = new MyDirectory(@"../../../../");
+
+            // Act
+            var result = directory.MakeFile("1.txt");
+            directory.Delete("1.txt");
+
+            // Assert
+            Assert.True(result);
+        }
+        
+        [Fact]
+        public void CreateFile_That_Already_Exist()
+        {
+            // Arrange
+            var directory = new MyDirectory(@"../../../../");
+
+            // Act
+            var result = directory.MakeFile("Program.cs");
+
+            // Assert
+            Assert.False(result);
         }
     }
-
+}
